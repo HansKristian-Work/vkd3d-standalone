b0908558890d6cd7d061ddab4ef7e80d3c33c8da
Hans-Kristian Arntzen <post@arntzen-software.no>
2020-01-21 19:50:31 +0100

    Dump byte-wise instead of words.
    DXIL doesn't align apparently.

--- dump_hex.c
+++ dump_hex.c
@@ -17,36 +17,15 @@ int main(int argc, char **argv)
     }
 
     unsigned count = 0;
-
-    for (;;)
+    uint8_t value;
+    while (fread(&value, 1, 1, file) == 1)
     {
-        union
+        printf("0x%02x,%s", value, count != 31 ? " " : "");
+        if (++count == 32)
         {
-            uint32_t u32;
-            uint8_t u8[4];
-        } u;
-        u.u32 = 0;
-
-        unsigned i;
-        for (i = 0; i < 4; i++)
-        {
-            if (fread(&u.u8[i], 1, 1, file) != 1)
-                break;
-
+            count = 0;
+            printf("\n");
         }
-
-        if (i > 0)
-        {
-            printf("0x%08x,%s", u.u32, count != 7 ? " " : "");
-            if (++count == 8)
-            {
-                count = 0;
-                printf("\n");
-            }
-        }
-
-        if (i < 4)
-            break;
     }
 
     fclose(file);
