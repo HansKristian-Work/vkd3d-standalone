52fc02df28e87408f2ca604aa867014f046c2225
Hans-Kristian Arntzen <post@arntzen-software.no>
2020-02-12 16:45:27 +0100

    Add 32-bit output option to dump-hex.

--- dump_hex.c
+++ dump_hex.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 2 && argc != 3)
     {
         fprintf(stderr, "Usage: dump-hex <DXBC path>\n");
         return 1;
     }
+
+    bool use_32bit = false;
+    if (argc == 3 && strtol(argv[2], NULL, 0) == 32)
+        use_32bit = true;
 
     FILE *file = fopen(argv[1], "rb");
     if (!file)
@@ -16,18 +22,35 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    unsigned count = 0;
-    uint8_t value;
-    while (fread(&value, 1, 1, file) == 1)
+    if (use_32bit)
     {
-        printf("0x%02x,%s", value, count != 31 ? " " : "");
-        if (++count == 32)
+        unsigned count = 0;
+        uint32_t value;
+        while (fread(&value, sizeof(value), 1, file) == 1)
         {
-            count = 0;
-            printf("\n");
+            printf("0x%08x,%s", value, count != 7 ? " " : "");
+            if (++count == 8)
+            {
+                count = 0;
+                printf("\n");
+            }
         }
     }
-
+    else
+    {
+        unsigned count = 0;
+        uint8_t value;
+        while (fread(&value, 1, 1, file) == 1)
+        {
+            printf("0x%02x,%s", value, count != 31 ? " " : "");
+            if (++count == 32)
+            {
+                count = 0;
+                printf("\n");
+            }
+        }
+    }
+    printf("\n");
     fclose(file);
     return 0;
 }
